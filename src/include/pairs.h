9a277b44650db66db331924f94dc90c6388dbaf6
Mathéo <matheo.piget5@gmail.com>
2025-04-16 11:35:33 +0200

    début fonction pour les pairs

--- src/include/pairs.h
+++ src/include/pairs.h
@@ -1,2 +1,10 @@
 #include <stdint.h>
 #include <netinet/in.h>
+
+typedef struct Pair
+{
+    int id;
+    struct in6_addr ip;
+    int port;
+    
+} Pair;
---
+++ src/pairs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/select.h>
+#include <arpa/inet.h>
+#include "include/pairs.h"
+#include "include/multicast.h"
+
+int init_pairs(){}
+
+int join_auction(){}
+
+int handle_join(int time){}
+
+int add_pair(Pair *pair){}
